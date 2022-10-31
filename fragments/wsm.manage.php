9ae226fec287f68882c166bed21c6dfccf153a52
Alexander Walther <aw@alexplus.de>
2022-10-31 03:22:49 +0100

    show consent_uuid and cookie consent content to user

--- fragments/wsm.manage.php
+++ fragments/wsm.manage.php
@@ -1,2 +1,41 @@
 <button type="button" data-cc="c-settings">Show cookie settings</button>
 <button type="button" data-cc="accept-all">Accept all cookies</button>
+
+<?php
+
+$current = rex::getServer();
+
+if (rex_addon::get('yrewrite')->isAvailable()) {
+    $current = rex_yrewrite::getCurrentDomain()->getName();
+};
+
+?>
+<p>Ihre Einwilligung trifft auf die folgenden Domains zu:
+	<mark><?= $current ?></mark>
+</p>
+
+<p>Ihr aktueller Zustand: <mark>
+		<script>
+			cookieconsent = initCookieConsent();
+
+			document.write(cookieconsent.get("categories"));
+		</script>
+	</mark> </p>
+<p>Ihre Einwilligungs-ID: <mark>
+		<script>
+			cookieconsent = initCookieConsent();
+
+			document.write(cookieconsent.get("consent_uuid"));
+		</script>
+	</mark></p>
+<p>Einwilligungsdatum: <mark>
+		<script>
+			cookieconsent = initCookieConsent();
+
+			document.write(cookieconsent.get("consent_date"));
+		</script>
+	</mark></p>
+
+
+<h2>
+</h2>
