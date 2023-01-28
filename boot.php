ff596d898318bc889b1d2aa753a03fcac75a4ee3
Alexander Walther <aw@alexplus.de>
2023-01-28 14:31:08 +0100

    load and show scripts

--- boot.php
+++ boot.php
@@ -20,5 +20,28 @@ if (rex_addon::get('yform')->isAvailable() && !rex::isSafeMode()) {
 };
 
 rex_extension::register('YFORM_DATA_ADDED', ['wsm','yform_data_added']);
-// rex_extension::register('YFORM_DATA_UPDATED', ['wsm','yform_data_added']);
+rex_extension::register('YFORM_DATA_UPDATED', ['wsm','yform_data_added']);
 rex_extension::register('YFORM_DATA_DELETED', ['wsm','yform_data_deleted']);
+
+if (rex::isBackend())
+{
+	rex_extension::register('YFORM_DATA_LIST', function( $ep ) {
+
+		if ($ep->getParam('table')->getTableName() == "rex_wenns_sein_muss_service"){
+			$list = $ep->getSubject();
+
+            $list->setColumnPosition('script', 3);
+			$list->setColumnFormat(
+				'script',
+				'custom',
+				function($a){
+                    if($a['list']->getValue('script') != "") {
+                        return "🍟 ja";
+                    } else {
+                        return "🥔";
+                    }
+				}
+			);
+		}
+	});
+}
--- fragments/wsm.script.php
+++ fragments/wsm.script.php
@@ -1,3 +1,3 @@
 <script type="text/plain" data-category="<?= $this->getVar('category') ?>" data-service="<?= $this->getVar('service') ?>">
-    <?= preg_replace("/<script.*?>(.*)<\/script>/s", "", $this->getVar('script')) ?>
+    <?= preg_replace('#<script[^>]*>([^<]+)</script>#', '$1',  $this->getVar('script')) ?>
 </script>
