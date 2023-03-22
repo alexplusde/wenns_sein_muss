d64fceefbdc9a5a88c19c5478bd5ab97b8e88e29
Alexander Walther <aw@alexplus.de>
2023-03-23 00:41:36 +0100

    update uninstall.php

--- uninstall.php
+++ uninstall.php
@@ -1,5 +1,7 @@
 <?php
 
 rex_config::removeNamespace("wenns_sein_muss");
-
-rex_sql::factory()->setQuery('DELETE FROM ' . rex_yform_manager_table::table() . ' where table_name="rex_wenns_sein_muss"');
+if (rex_addon::get('yform')->isAvailable() && !rex::isSafeMode()) {
+    //  rex_sql::factory()->setQuery('DELETE FROM ' . rex_yform_manager_table::table() . ' where table_name="rex_wenns_sein_muss"');
+    // rex_yform_manager_table::deleteCache();
+}
