04797c5f02359e21f80001e4fafc0b9797bd3c45
Alexander Walther <aw@alexplus.de>
2023-03-25 09:31:23 +0100

    YForm-Tabellen beim Deinstallieren löschen

--- uninstall.php
+++ uninstall.php
@@ -2,6 +2,10 @@
 
 rex_config::removeNamespace("wenns_sein_muss");
 if (rex_addon::get('yform')->isAvailable() && !rex::isSafeMode()) {
-    //  rex_sql::factory()->setQuery('DELETE FROM ' . rex_yform_manager_table::table() . ' where table_name="rex_wenns_sein_muss"');
-    // rex_yform_manager_table::deleteCache();
+    rex_yform_manager_table_api::removeTable('wenns_sein_muss_domain');
+    rex_yform_manager_table_api::removeTable('wenns_sein_muss_entry');
+    rex_yform_manager_table_api::removeTable('wenns_sein_muss_group');
+    rex_yform_manager_table_api::removeTable('wenns_sein_muss_service');
+    rex_yform_manager_table_api::removeTable('wenns_sein_muss_iframe');
+    rex_yform_manager_table_api::removeTable('wenns_sein_muss_protocol');
 }
