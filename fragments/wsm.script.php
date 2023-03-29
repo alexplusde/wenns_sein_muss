7e76826304e52a1d14955e8e8cd7a5e95d8fd3f4
Alexander Walther <aw@alexplus.de>
2023-03-29 18:21:18 +0200

    normalize service to stop double listing of services

--- fragments/wsm.script.php
+++ fragments/wsm.script.php
@@ -1,3 +1,9 @@
-<script type="text/plain" data-category="<?= $this->getVar('category') ?>" data-service="<?= $this->getVar('service') ?>">
-    <?= preg_replace('#<script[^>]*>([^<]+)</script>#', '$1',  $this->getVar('script')) ?>
+<script type="text/plain"
+	data-category="<?= $this->getVar('category') ?>"
+	data-service="<?= rex_string::normalize($this->getVar('service')) ?>">
+	<?= preg_replace(
+    '#<script[^>]*>([^<]+)</script>#',
+    '$1',
+    $this->getVar('script')
+) ?>
 </script>
