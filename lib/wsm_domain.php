0bed4699e8ef302ef358587adb1e2f9cee76de15
Alexander Walther <aw@alexplus.de>
2023-03-29 15:06:24 +0200

    fix wsm_domain::getCurrent()

--- lib/wsm_domain.php
+++ lib/wsm_domain.php
@@ -3,7 +3,7 @@ class wsm_domain extends \rex_yform_manager_dataset
 {
     public static function getCurrent()
     {
-        return wsm_domain::get(wsm::getDomainId());
+        return wsm_domain::query()->where('domain_id', wsm::getDomainId())->findOne();
     }
 
     public function getImprintArticle()
