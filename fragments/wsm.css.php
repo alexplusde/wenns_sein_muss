41d6d66fb7fea9fdac2139e8c9e4b4f6e1f74c6f
Alexander Walther <aw@alexplus.de>
2023-03-29 18:20:56 +0200

    include js in css fragment

--- fragments/wsm.css.php
+++ fragments/wsm.css.php
@@ -3,3 +3,7 @@
 	media="print" onload="this.media='all'">
 <link rel="stylesheet"
 	href="<?= rex_url::addonAssets('wenns_sein_muss', 'iframemanager/iframemanager.css') ?>">
+<script defer src="/assets/addons/wenns_sein_muss/cookieconsent/cookieconsent.js">
+</script>
+<script defer src="/assets/addons/wenns_sein_muss/iframemanager/iframemanager.js">
+</script>
