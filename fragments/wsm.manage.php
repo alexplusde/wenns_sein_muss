abf4aec3d7762b04ccffa7052a60be16267619b6
Alexander Walther <aw@alexplus.de>
2024-08-26 15:39:05 +0200

    Doppelten Button entfernen

--- fragments/wsm.manage.php
+++ fragments/wsm.manage.php
@@ -1,6 +1,2 @@
 <button type="button" class="btn btn-primary"
 	data-cc="show-preferencesModal"><?= wsm::getConfigText("consent_settings_open") ?></button>
-<!-- Development Purposes -->
-<button class="btn btn-secondary" type="button"
-	data-cc="show-consentModal"><?= wsm::getConfigText("consent_modal_open") ?></button>
-<!-- Test -->
