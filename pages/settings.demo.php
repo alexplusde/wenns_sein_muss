5c1e664c44502ccfd99c3c0fef06ab9a3edfc127
Alexander Walther <mail@alexplus.de>
2024-08-30 22:58:18 +0200

    Texte durch Sprog-Keys austauschen
    #22

--- pages/settings.demo.php
+++ pages/settings.demo.php
@@ -13,8 +13,19 @@ if ($func !== '') {
             rex_sql_util::importDump($file);
             echo rex_view::success(rex_i18n::msg('wenns_sein_muss_demo_imported'));
         }
+        if ($func === 'sprog') {
+            $package = rex_package::get('wenns_sein_muss');
+            $sprog_config = $package->getProperty('sprog_config');
+            foreach($sprog_config as $key) {
+                rex_config::set('wenns_sein_muss', $key, Sprog\Wildcard::getOpenTag() . $key .Sprog\Wildcard::getCloseTag());
+            }
+        }
+            
     }
 }
+
+/* Demo-Daten importieren */
+
 $content = "";
 $content .= '<p>'.rex_i18n::msg('wenns_sein_muss_demo_import').'</p>';
 $content .= '<p><a class="btn btn-primary" href="'.rex_url::currentBackendPage(['func' => 'setup'] + $csrf->getUrlParams()).'" data-confirm="'.rex_i18n::msg('wenns_sein_muss_demo_warning').'">'.rex_i18n::msg('wenns_sein_muss_demo').'</a></p>';
@@ -24,3 +35,18 @@ $fragment->setVar('class', 'danger', false);
 $fragment->setVar('title', rex_i18n::msg('wenns_sein_muss_demo'), false);
 $fragment->setVar('body', $content, false);
 echo $fragment->parse('core/page/section.php');
+
+/* Config mit Sprog-Keys überschreiben  */
+
+if(rex_addon::get('sprog')->isAvailable()) {
+    $content = "";
+    $content .= '<p>'.rex_i18n::msg('wenns_sein_muss_sprog_import').'</p>';
+    $content .= '<p><a class="btn btn-primary" href="'.rex_url::currentBackendPage(['func' => 'sprog'] + $csrf->getUrlParams()).'" data-confirm="'.rex_i18n::msg('wenns_sein_muss_sprog_warning').'">'.rex_i18n::msg('wenns_sein_muss_sprog').'</a></p>';
+
+
+    $fragment = new rex_fragment();
+    $fragment->setVar('class', 'danger', false);
+    $fragment->setVar('title', rex_i18n::msg('wenns_sein_muss_sprog'), false);
+    $fragment->setVar('body', $content, false);
+    echo $fragment->parse('core/page/section.php');
+}
