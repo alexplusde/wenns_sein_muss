14e0a1b9538b0e429cca7d8d160e0a5a06e1721e
alxndr-w <alxndr-w@users.noreply.github.com>
2024-09-13 15:43:31 +0000

    Apply php-cs-fixer changes

--- lib/Api/Iframe.php
+++ lib/Api/Iframe.php
@@ -3,11 +3,13 @@
 namespace Alexplusde\Wsm;
 
 use rex_api_function;
+use rex_file;
+use rex_logger;
+use rex_path;
 use rex_response;
 use rex_socket;
+use rex_socket_exception;
 use rex_string;
-use rex_path;
-use rex_file;
 use rex_url;
 
 use function array_key_exists;
@@ -32,7 +34,7 @@ class ApiWsmIframe extends rex_api_function
                 // liest die Informationen aus der Datei
                 $body = json_decode($response->getBody());
             }
-            if (is_array($body) && array_key_exists(0, $body) && property_exists($body[0], 'thumbnail_large')) {                
+            if (is_array($body) && array_key_exists(0, $body) && property_exists($body[0], 'thumbnail_large')) {
                 $url = self::getImgFromVimeo($id, $body[0]->thumbnail_large);
                 rex_response::setStatus('301');
                 rex_response::sendRedirect($url);
@@ -48,11 +50,9 @@ class ApiWsmIframe extends rex_api_function
         exit;
     }
 
-    
-    private static function getImgFromYoutube(string $id) :string
+    private static function getImgFromYoutube(string $id): string
     {
-        
-        if (null !== \rex_file::get(rex_path::addonData('wenns_sein_muss', self::generateFileName('youtube', $id)))) {
+        if (null !== rex_file::get(rex_path::addonData('wenns_sein_muss', self::generateFileName('youtube', $id)))) {
             return self::getThumbUrl('youtube', $id);
         }
 
@@ -60,7 +60,7 @@ class ApiWsmIframe extends rex_api_function
             $socket = rex_socket::factory('i3.ytimg.com', 443, true);
             $socket->setPath('/vi/' . $id . '/hqdefault.jpg');
             $socket->followRedirects(1);
-            
+
             $response = $socket->doGet();
 
             if ($response->isOk()) {
@@ -68,16 +68,15 @@ class ApiWsmIframe extends rex_api_function
                 $response->writeBodyTo($image_path);
                 return self::getThumbUrl('youtube', $id);
             }
-        } catch(\rex_socket_exception $e) {
-            \rex_logger::factory()->notice($e->getMessage());
+        } catch (rex_socket_exception $e) {
+            rex_logger::factory()->notice($e->getMessage());
         }
         return '';
     }
 
-    private static function getImgFromVimeo(string $id, string $url) :string
+    private static function getImgFromVimeo(string $id, string $url): string
     {
-        
-        if (null !== \rex_file::get(rex_path::addonData('wenns_sein_muss', self::generateFileName('vimeo', $id)))) {
+        if (null !== rex_file::get(rex_path::addonData('wenns_sein_muss', self::generateFileName('vimeo', $id)))) {
             return self::getThumbUrl('vimeo', $id);
         }
 
@@ -90,24 +89,24 @@ class ApiWsmIframe extends rex_api_function
                 $response->writeBodyTo($image_path);
                 return self::getThumbUrl('vimeo', $id);
             }
-        } catch(\rex_socket_exception $e) {
-            \rex_logger::factory()->notice($e->getMessage());
+        } catch (rex_socket_exception $e) {
+            rex_logger::factory()->notice($e->getMessage());
         }
         return '';
     }
 
-    private static function generateFileName(string $service = '', string $id, string $filetype = '.jpg') :string
+    private static function generateFileName(string $service, string $id, string $filetype = '.jpg'): string
     {
         return rex_string::normalize($service . '_' . $id) . $filetype;
     }
-    
-    public static function getThumbUrl(string $service = '', string $id, string $filetype = '.jpg') :string
+
+    public static function getThumbUrl(string $service, string $id, string $filetype = '.jpg'): string
     {
         $filename = self::generateFilename($service, $id, $filetype);
         $file = rex_path::addonData('wenns_sein_muss', $filename);
 
         $timestamp = filemtime($file);
-        $frontend_url = rex_url::media(). Wsm::getConfig('media_manager_type', 'string', 'wsm').'/'.$filename.'?timestamp='.$timestamp;
+        $frontend_url = rex_url::media() . Wsm::getConfig('media_manager_type', 'string', 'wsm') . '/' . $filename . '?timestamp=' . $timestamp;
         return $frontend_url;
     }
 }
